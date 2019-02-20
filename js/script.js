bd6beac10d1619e87b3fe08273f35f1d003d5468
igordefo <dekleidung@gmail.com>
2019-02-20 19:20:56 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -1,35 +1,17 @@
-ymaps.ready(function () {
-    var map = new ymaps.Map('map', {
-      center: [59.939390, 30.329545],
-      zoom: 16,
-      scrollZoom: false,//зум при скролле мышью отключен
-      controls:[]//элементы контролов отсутствуют (строка поиска, кнопки зума, кнопки типа карты и тд)
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    Placemark = new ymaps.Placemark([59.938706, 30.323149], {
-      balloonContent: 'Санкт-Петербург, Большая Конюшенная улица, 19/8'
-    }, {
-      // Опции.
-      iconLayout: 'default#image',
-      // Своё изображение иконки метки.
-      iconImageHref: 'img/icons/pin.svg',
-      // Размеры метки.
-      iconImageSize: [80, 140],
-      // Смещение левого верхнего угла иконки относительно
-      // её "ножки" (точки привязки).
-      iconImageOffset: [-40, -140],
-      // Добавляем иконку тени и задаем аналогичные параметры.
-      iconShadow: true,
-      iconShadowLayout: 'default#image',
-      iconShadowImageHref: 'img/icons/pin-shadow.png',
-      iconShadowImageSize: [182, 110],
-      iconShadowImageOffset: [0, -110]
-    });
+// Map
 
-    map.geoObjects.add(Placemark);
-    map.behaviors.disable('scrollZoom');
-    map.controls.add('zoomControl');
-    var roadcontrolState = map.controls.get('zoomControl').state.get('size');
-    map.controls.get('zoomControl').options.set('size', 'small');
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(59.9992618,30.3648466)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+  var image = "http://i.neoseeker.com/mgv/499846/846/33/15847_179440563793_179437393793_2772323_4548598_n_icon.jpg";
+  var myLatLng = new google.maps.LatLng(59.999,30.364);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
   });
+}
